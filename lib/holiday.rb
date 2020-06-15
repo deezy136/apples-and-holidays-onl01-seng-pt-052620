17a179eeec90fcbed3ab825eb54af18b5afbbd44
Clyde Griffin <deezy13@gmail.com>
2020-06-15 17:21:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,9 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   
-  holiday_hash.each do |season, holiday|
-    
-    if season
+  holiday_supplies[:summer][:fourth_of_july][1]
  
   
 end 
